2e488a5427998f78fba4d8dde3542514a9748cd8
AtiSarah <sarah.atif@esst-sup.com>
2024-05-19 16:31:19 +0100

    .

--- php/driver/driver.php
+++ php/driver/driver.php
@@ -27,7 +27,6 @@ $mission_sql = $link->prepare("SELECT * FROM mission WHERE id_driver = ?");
 $mission_sql->bind_param("i", $id_driver);
 $mission_sql->execute();
 $mission_result = $mission_sql->get_result();
-$_SESSION["id_mission"] = $mission_result['id_mission'];
 ?>
 
 
@@ -103,7 +102,7 @@ $_SESSION["id_mission"] = $mission_result['id_mission'];
       <span class="text"></span>
     </div>
 <!-- Dashboard driver-->
- <div id="Dashboard-driver">
+ <div class="Dashboard-driver">
         <table border="1"> 
             <thead>
                 <tr>
--- php/driver/incidentals.php
+++ php/driver/incidentals.php
@@ -1,4 +1,3 @@
-
 <?php
 session_start();
 include("../dbp.php"); 
@@ -7,151 +6,111 @@ if (!isset($_SESSION['user_id'])) {
     header("Location: ../error.php");
     exit();
 }
-if (!isset($_SESSION['id_mission'])) {
-  header("Location: mission.php");
-}
 $id = $_SESSION['user_id'];
 $sql = $link->prepare("SELECT * FROM driver WHERE id = ?");
 $sql->bind_param("i", $id);
 $sql->execute();
 $result = $sql->get_result();
 $row = $result->fetch_assoc();
-
 ?>
 
-
 <!DOCTYPE html>
-<!-- Coding by CodingNepal | www.codingnepalweb.com -->
 <html lang="en" dir="ltr">
-  <head>
+<head>
     <meta charset="UTF-8">
     <title>Account</title>
     <link rel="stylesheet" href="incidentals.css">
-    <!-- Boxiocns CDN Link -->
+    <!-- Boxicons CDN Link -->
     <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   </head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
 <body>
-  <div class="sidebar close">
+<div class="sidebar close">
     <div class="logo-details">
-    <i class="#"></i>
-      <span class="logo_name">VROOMCAR</span>
+        <i class="#"></i>
+        <span class="logo_name">VROOMCAR</span>
     </div>
     <ul class="nav-links">
-      <li>
-        <a href="driver.php"><!-- DASHBOARD-->
-          <i class='bx bx-grid-alt' ></i>
-          <span class="link_name">Dashboard</span>
-        </a>
-        <ul class="sub-menu blank">
-          <li><a class="link_name" href="driver.php">Dashboard</a></li>
-        </ul>
-      </li>
-      <li>
-        <div class="iocn-link">
-          <a href="mission.php">
-            <i class='bx bx-collection' ></i>
-            <span class="link_name">Mission</span>
-          </a>
-          <i class='bx bxs-chevron-down arrow' ></i>
-        </div>
-        <ul class="sub-menu">
-          <li><a href="mission.php">View Mission</a></li><!-- MISSION-->
-          <li><a href="incidentals.php">Add Incidentals</a></li><!-- ADD INCIDENTALS-->
-        </ul>
-      </li>
-      <li>
-      <a href="account.php"><!-- ACCOUNT-->
-            <i class="fa-solid fa-user"></i>
-          <span class="link_name">Account</span>
-        </a>
-        <ul class="sub-menu blank">
-          <li><a class="link_name" href="account.php">Account</a></li>
-        </ul>
-      </li>
-      <li>
-    <div class="profile-details">
-      <div class="profile-content">
-      <img src="../../html/img/driver.PNG" alt="profileImg">
-      </div>
-      <div class="name-job">
-      <h3 id="name"></h3>
-        <div class="profile_name"><?php echo $row['firstname'] . ' ' . $row['lastname']; ?></div>
-        <div class="job">Driver</div>
-      </div>
-<a href="../disconnect.php" class="logout-link">
-    <i class='bx bx-log-out'></i>
-</a>
-    </div>
-  </li>
-</ul>
-  </div>
-  <section class="home-section">
+        <li>
+            <a href="driver.php">
+                <i class='bx bx-grid-alt' ></i>
+                <span class="link_name">Dashboard</span>
+            </a>
+            <ul class="sub-menu blank">
+                <li><a class="link_name" href="driver.php">Dashboard</a></li>
+            </ul>
+        </li>
+        <li>
+            <div class="iocn-link">
+                <a href="mission.php">
+                    <i class='bx bx-collection' ></i>
+                    <span class="link_name">Mission</span>
+                </a>
+                <i class='bx bxs-chevron-down arrow' ></i>
+            </div>
+            <ul class="sub-menu">
+                <li><a href="mission.php">View Mission</a></li>
+                <li><a href="incidentals.php">Add Incidentals</a></li>
+            </ul>
+        </li>
+        <li>
+            <a href="account.php">
+                <i class="fa-solid fa-user"></i>
+                <span class="link_name">Account</span>
+            </a>
+            <ul class="sub-menu blank">
+                <li><a class="link_name" href="account.php">Account</a></li>
+            </ul>
+        </li>
+        <li>
+            <div class="profile-details">
+                <div class="profile-content">
+                    <img src="../../html/img/driver.PNG" alt="profileImg">
+                </div>
+                <div class="name-job">
+                    <h3 id="name"></h3>
+                    <div class="profile_name"><?php echo $row['firstname'] . ' ' . $row['lastname']; ?></div>
+                    <div class="job">Driver</div>
+                </div>
+                <a href="../disconnect.php" class="logout-link">
+                    <i class='bx bx-log-out'></i>
+                </a>
+            </div>
+        </li>
+    </ul>
+</div>
+<section class="home-section">
     <div class="home-content">
-      <i class='bx bx-menu' ></i>
-      <span class="text"></span>
+        <i class='bx bx-menu' ></i>
+        <span class="text"></span>
     </div>
     <!-- Frais-imprevus-->
     <div id="frais-imprevus">
-    <form action="post" name="imprevus">
-        <legend><strong>Incidentals:</strong></legend>
-        <input type="text" placeholder="enter incidentals..."><br>
-        <input type="number" placeholder="enter amount..."><br>
-        <input type="submit" value="Send">&nbsp;
-        <input type="reset" value="Cancel">
-    </form>
+        <form action="process_incidentals.php" method="post">
+            <legend><strong>Incidentals:</strong></legend>
+            <input type="text" name="incidentals" placeholder="Enter incidentals..." required><br>
+            <input type="number" name="amount" placeholder="Enter amount..." required><br>
+            <input type="submit" name="submit" value="Send">&nbsp;
+            <input type="reset" value="Cancel">
+        </form>
     </div>
-</div>
-  </section>
+</section>
 
- 
-
-  <script>
-  let arrow = document.querySelectorAll(".arrow");
-  for (var i = 0; i < arrow.length; i++) {
-    arrow[i].addEventListener("click", (e)=>{
-   let arrowParent = e.target.parentElement.parentElement;//selecting main parent of arrow
-   arrowParent.classList.toggle("showMenu");
+<script>
+    let arrow = document.querySelectorAll(".arrow");
+    for (var i = 0; i < arrow.length; i++) {
+        arrow[i].addEventListener("click", (e)=>{
+            let arrowParent = e.target.parentElement.parentElement; //selecting main parent of arrow
+            arrowParent.classList.toggle("showMenu");
+        });
+    }
+    let sidebar = document.querySelector(".sidebar");
+    let sidebarBtn = document.querySelector(".bx-menu");
+    console.log(sidebarBtn);
+    sidebarBtn.addEventListener("click", ()=>{
+        sidebar.classList.toggle("close");
     });
-  }
-  let sidebar = document.querySelector(".sidebar");
-  let sidebarBtn = document.querySelector(".bx-menu");
-  console.log(sidebarBtn);
-  sidebarBtn.addEventListener("click", ()=>{
-    sidebar.classList.toggle("close");
-  });
-  </script>
+</script>
 </body>
-
 </html>
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
