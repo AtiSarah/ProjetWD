8e10782a7d4fe92937146b634f88ed61e6cd59d7
AtiSarah <sarah.atif@esst-sup.com>
2024-05-19 15:10:35 +0100

    Update driver.php

--- php/driver/driver.php
+++ php/driver/driver.php
@@ -1,4 +1,3 @@
-
 <?php
 session_start();
 include("../dbp.php"); 
@@ -7,26 +6,41 @@ if (!isset($_SESSION['profile1'])) {
   exit();
 }
 $id = $_SESSION['user_id'];
+
+// Récupérer l'id_driver associé à l'utilisateur connecté
+$sql1 = $link->prepare("SELECT id_driver FROM driver WHERE id = ?");
+$sql1->bind_param("i", $id);
+$sql1->execute();
+$result1 = $sql1->get_result();
+$rowW = $result1->fetch_assoc();
+$id_driver = $rowW['id_driver'];
+
+// Récupérer les informations de l'utilisateur
 $sql = $link->prepare("SELECT * FROM driver WHERE id = ?");
 $sql->bind_param("i", $id);
 $sql->execute();
 $result = $sql->get_result();
 $row = $result->fetch_assoc();
+
+// Récupérer les missions pour le conducteur connecté
+$mission_sql = $link->prepare("SELECT * FROM mission WHERE id_driver = ?");
+$mission_sql->bind_param("i", $id_driver);
+$mission_sql->execute();
+$mission_result = $mission_sql->get_result();
 ?>
 
 
 <!DOCTYPE html>
-<!-- Coding by CodingNepal | www.codingnepalweb.com -->
 <html lang="en" dir="ltr">
-  <head>
-    <meta charset="UTF-8">
-    <title>Account</title>
-    <link rel="stylesheet" href="driver.css">
-    <!-- Boxiocns CDN Link -->
-    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   </head>
+<head>
+  <meta charset="UTF-8">
+  <title>Account</title>
+  <link rel="stylesheet" href="driver.css">
+  <!-- Boxiocns CDN Link -->
+  <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
 <body>
   <div class="sidebar close">
     <div class="logo-details">
@@ -72,7 +86,7 @@ $row = $result->fetch_assoc();
       </div>
       <div class="name-job">
       <h3 id="name"></h3>
-        <div class="profile_name"><?php echo $row['firstname'] . ' ' . $row['lastname']; ?></div>
+        <div class="profile_name"><?php echo htmlspecialchars($row['firstname'] . ' ' . $row['lastname']); ?></div>
         <div class="job">Driver</div>
       </div>
 <a href="../disconnect.php" class="logout-link">
@@ -102,43 +116,39 @@ $row = $result->fetch_assoc();
                 </tr>
             </thead>  
             <tbody>
+                <?php while ($mission_row = $mission_result->fetch_assoc()): ?>
                 <tr>
-                    <td><!-- Mission Type content --></td> 
-                    <td><!-- Departure City content --></td>
-                    <td><!-- Arrival City content --></td>
-                    <td><!-- Departure Date content --></td>
-                    <td><!-- Duration content --></td>
-                    <td><!-- Cost content --></td>
-                    <td><!-- Vehicle content --></td>
+                    <td><?php echo htmlspecialchars($mission_row['departure_city']); ?></td>
+                    <td><?php echo htmlspecialchars($mission_row['arrival_city']); ?></td>
+                    <td><?php echo htmlspecialchars($mission_row['departure_date']); ?></td>
+                    <td><?php echo htmlspecialchars($mission_row['duration']); ?></td>
+                    <td><?php echo htmlspecialchars($mission_row['cost']); ?></td>
+                    <td><?php echo htmlspecialchars($mission_row['type']); ?></td>
+                    <!-- You need to fetch and display the vehicle details for this mission -->
+                    <td><?php echo htmlspecialchars($mission_row['id_vehicle']); ?></td>
                 </tr>
-                <!-- More mission entries can be added here -->
+                <?php endwhile; ?>
             </tbody>
         </table>
        <button>Start</button>
     </div>
   </section>
-
- 
-
   <script>
   let arrow = document.querySelectorAll(".arrow");
   for (var i = 0; i < arrow.length; i++) {
-    arrow[i].addEventListener("click", (e)=>{
-   let arrowParent = e.target.parentElement.parentElement;//selecting main parent of arrow
-   arrowParent.classList.toggle("showMenu");
+    arrow[i].addEventListener("click", (e) => {
+      let arrowParent = e.target.parentElement.parentElement; //selecting main parent of arrow
+      arrowParent.classList.toggle("showMenu");
     });
   }
+
   let sidebar = document.querySelector(".sidebar");
   let sidebarBtn = document.querySelector(".bx-menu");
   console.log(sidebarBtn);
-  sidebarBtn.addEventListener("click", ()=>{
+  sidebarBtn.addEventListener("click", () => {
     sidebar.classList.toggle("close");
   });
-  </script>
+</script>
+
 </body>
-
 </html>
-
-    
-
-
