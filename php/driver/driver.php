b39d87ea06cf75301211d46a377ab2d1171a1615
AtiSarah <sarah.atif@esst-sup.com>
2024-05-20 16:53:28 +0100

    Update driver.php

--- php/driver/driver.php
+++ php/driver/driver.php
@@ -1,13 +1,14 @@
 <?php
 session_start();
 include("../dbp.php"); 
+
 if (!isset($_SESSION['profile1'])) {
   header("Location: ../error.php");
   exit();
 }
 $id = $_SESSION['user_id'];
 
-// Récupérer l'id_driver associé à l'utilisateur connecté
+// Get the id_driver associated with the logged-in user
 $sql1 = $link->prepare("SELECT id_driver FROM driver WHERE id = ?");
 $sql1->bind_param("i", $id);
 $sql1->execute();
@@ -15,15 +16,15 @@ $result1 = $sql1->get_result();
 $rowW = $result1->fetch_assoc();
 $id_driver = $rowW['id_driver'];
 
-// Récupérer les informations de l'utilisateur
+// Get the user's information
 $sql = $link->prepare("SELECT * FROM driver WHERE id = ?");
 $sql->bind_param("i", $id);
 $sql->execute();
 $result = $sql->get_result();
 $row = $result->fetch_assoc();
 
-// Récupérer les missions pour le conducteur connecté
-$mission_sql = $link->prepare("SELECT * FROM mission WHERE id_driver = ?");
+// Get missions for the connected driver where finish = 0
+$mission_sql = $link->prepare("SELECT * FROM mission WHERE id_driver = ? AND finish = 0");
 $mission_sql->bind_param("i", $id_driver);
 $mission_sql->execute();
 $mission_result = $mission_sql->get_result();
@@ -36,7 +37,7 @@ $mission_result = $mission_sql->get_result();
   <meta charset="UTF-8">
   <title>Account</title>
   <link rel="stylesheet" href="driver.css">
-  <!-- Boxiocns CDN Link -->
+  <!-- Boxicons CDN Link -->
   <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -124,8 +125,18 @@ $mission_result = $mission_sql->get_result();
                     <td><?php echo htmlspecialchars($mission_row['arrival_date']); ?></td>
                     <td><?php echo htmlspecialchars($mission_row['cost']); ?></td>
                     <td><?php echo htmlspecialchars($mission_row['type']); ?></td>
-                    <!-- You need to fetch and display the vehicle details for this mission -->
-                    <td><?php echo htmlspecialchars($mission_row['id_vehicle']); ?></td>
+                    <!-- Fetch and display the vehicle immatriculation -->
+                    <td>
+                        <?php
+                        $vehicle_id = $mission_row['id_vehicle'];
+                        $vehicle_sql = $link->prepare("SELECT immatriculation FROM vehicle WHERE id_vehicle = ?");
+                        $vehicle_sql->bind_param("i", $vehicle_id);
+                        $vehicle_sql->execute();
+                        $vehicle_result = $vehicle_sql->get_result();
+                        $vehicle_row = $vehicle_result->fetch_assoc();
+                        echo htmlspecialchars($vehicle_row['immatriculation']);
+                        ?>
+                    </td>
                 </tr>
                 <?php endwhile; ?>
             </tbody>
