b77970eb4ee69dbdbed5447f42b296e46237e977
imenepiu <imeneben116@gmail.com>
2024-05-21 10:09:23 +0100

    Update dbp.php

--- php/dbp.php
+++ php/dbp.php
@@ -4,28 +4,12 @@ $host = "phpmyadmin.alwaysdata.com";
 $dbuser = "root";
 $dbpass = "Azerty";
 $dbname = "vroomcar";
-
 // Create database connection
 $link = mysqli_connect($host, $dbuser, $dbpass, $dbname);
-
 // Check connection
-if(!$link) {
-    die("Connection failed: " . mysqli_connect_error());
+if(mysqli_connect_error())
+{
+echo "Connection establishing failed! <br >";
 }
 
-// Example query to fetch data
-$query = "SELECT * FROM your_table_name";
-$result = mysqli_query($link, $query);
-
-// Fetch data and output as JSON
-$data = array();
-while($row = mysqli_fetch_assoc($result)) {
-    $data[] = $row;
-}
-
-header('Content-Type: application/json');
-echo json_encode($data);
-
-// Close connection
-mysqli_close($link);
 ?>
